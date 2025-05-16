ebb63316d1ef221be9c536a3914de23f360c42c7
kume07 <49756039+kume07@users.noreply.github.com>
2025-05-16 19:18:25 +0300

    Update homework_7_1.py

--- homework_7_1.py
+++ homework_7_1.py
@@ -1,4 +1,6 @@
 def introduce_person(name: str, age: int) -> str:
+    assert isinstance(name, str) and name.strip() != ""
+    assert isinstance(age, int) and age > 0
     return f"Hi. My name is {name} and I am {age}."
 
 
