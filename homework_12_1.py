4ac7563e7d886a62225f446aa81339be49977885
kume07 <kumaninho@gmail.com>
2025-06-03 20:26:25 +0300

    Homework 12.1 Function delete_html_tags

--- homework_12_1.py
+++ homework_12_1.py
@@ -2,11 +2,10 @@ import re
 import codecs
 
 
-def delete_html_tags(html_file, result_file="cleaned.txt"):
+def delete_html_tags(html_file, result_file="cleaned.txt") -> None:
     with codecs.open(html_file, "r", "utf-8") as file:
         html = file.read()
 
-    # Видаляємо всі HTML-теги за допомогою регулярного виразу
     cleaned_text = re.sub(r"<[^>]+>", "", html)
 
     with codecs.open(result_file, "w", "utf-8") as file:
